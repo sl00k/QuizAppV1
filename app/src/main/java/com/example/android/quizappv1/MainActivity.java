a89a7b4fde000ef8b6b5ff08f89a801dd87206c7
sl00k <morcra@gmx.de>
2018-04-01 12:29:15 +0200

    added bool variable to determine if the score was calculated before

--- app/src/main/java/com/example/android/quizappv1/MainActivity.java
+++ app/src/main/java/com/example/android/quizappv1/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public int scoreSummary = 0;
+    public int scoreSummary;
     RadioGroup radioGroup1;
     RadioGroup radioGroup2;
     RadioGroup radioGroup4;
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private int answer8 = 0;
     private int answer9 = 0;
     private EditText question5_input;
+    private boolean hasRated = false;
 
 
     @Override
@@ -56,93 +57,103 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getScore(View view) {
-        // Question 1
-        // find which radio button is selected
-        int checkedId = radioGroup1.getCheckedRadioButtonId();
-        View selected = radioGroup1.findViewById(checkedId);
-        int selectedAnswer = radioGroup1.indexOfChild(selected);
-        if (selectedAnswer == answer1) {
-            scoreSummary += score;
-            disableBtn(radioGroup1);
-        } else disableBtn(radioGroup1);
-        // Question 2
-        // find which radio button is selected
-        checkedId = radioGroup2.getCheckedRadioButtonId();
-        selected = radioGroup2.findViewById(checkedId);
-        selectedAnswer = radioGroup2.indexOfChild(selected);
-        if (selectedAnswer == answer2) {
-            scoreSummary += score;
-            disableBtn(radioGroup2);
-        } else disableBtn(radioGroup2);
-        // Question 3
-        if (checkBoxGold.isChecked() && checkBoxIron.isChecked() && checkBoxCopper.isChecked() && !checkBoxSteel.isChecked()) {
-            scoreSummary += score;
-            checkBoxSteel.setEnabled(false);
-            checkBoxGold.setEnabled(false);
-            checkBoxIron.setEnabled(false);
-            checkBoxCopper.setEnabled(false);
+        if (hasRated == false) {
+            // Question 1
+            // find which radio button is selected
+            int checkedId = radioGroup1.getCheckedRadioButtonId();
+            View selected = radioGroup1.findViewById(checkedId);
+            int selectedAnswer = radioGroup1.indexOfChild(selected);
+            if (selectedAnswer == answer1) {
+                scoreSummary += score;
+                disableBtn(radioGroup1);
+            } else disableBtn(radioGroup1);
+            // Question 2
+            // find which radio button is selected
+            checkedId = radioGroup2.getCheckedRadioButtonId();
+            selected = radioGroup2.findViewById(checkedId);
+            selectedAnswer = radioGroup2.indexOfChild(selected);
+            if (selectedAnswer == answer2) {
+                scoreSummary += score;
+                disableBtn(radioGroup2);
+            } else disableBtn(radioGroup2);
+            // Question 3
+            if (checkBoxGold.isChecked() && checkBoxIron.isChecked() && checkBoxCopper.isChecked() && !checkBoxSteel.isChecked()) {
+                scoreSummary += score;
+                checkBoxSteel.setEnabled(false);
+                checkBoxGold.setEnabled(false);
+                checkBoxIron.setEnabled(false);
+                checkBoxCopper.setEnabled(false);
+            } else {
+                checkBoxSteel.setEnabled(false);
+                checkBoxGold.setEnabled(false);
+                checkBoxIron.setEnabled(false);
+                checkBoxCopper.setEnabled(false);
+            }
+            // Question 4
+            // find which radio button is selected
+            checkedId = radioGroup4.getCheckedRadioButtonId();
+            selected = radioGroup4.findViewById(checkedId);
+            selectedAnswer = radioGroup4.indexOfChild(selected);
+            if (selectedAnswer == answer4) {
+                scoreSummary += score;
+                disableBtn(radioGroup4);
+            } else disableBtn(radioGroup4);
+            // Question 5
+            // Compare Input string with correct answer
+            EditText text = (EditText) findViewById(R.id.question5_input);
+            String question5Input = text.getText().toString();
+            question5Input = question5Input.trim();
+            String correctAnswer5 = getResources().getString(R.string.question_5a);
+            if (question5Input.equalsIgnoreCase(correctAnswer5)) {
+                scoreSummary += score;
+                question5_input.setEnabled(false);
+            } else question5_input.setEnabled(false);
+            // Question 6
+            // find which radio button is selected
+            checkedId = radioGroup6.getCheckedRadioButtonId();
+            selected = radioGroup6.findViewById(checkedId);
+            selectedAnswer = radioGroup6.indexOfChild(selected);
+            if (selectedAnswer == answer6) {
+                scoreSummary += score;
+                disableBtn(radioGroup6);
+            } else disableBtn(radioGroup6);
+            // Question 7
+            // find which radio button is selected
+            checkedId = radioGroup7.getCheckedRadioButtonId();
+            selected = radioGroup7.findViewById(checkedId);
+            selectedAnswer = radioGroup7.indexOfChild(selected);
+            if (selectedAnswer == answer7) {
+                scoreSummary += score;
+                disableBtn(radioGroup7);
+            } else disableBtn(radioGroup7);
+            // Question 8
+            // find which radio button is selected
+            checkedId = radioGroup8.getCheckedRadioButtonId();
+            selected = radioGroup8.findViewById(checkedId);
+            selectedAnswer = radioGroup8.indexOfChild(selected);
+            if (selectedAnswer == answer8) {
+                scoreSummary += score;
+                disableBtn(radioGroup8);
+            } else disableBtn(radioGroup8);
+            // Question 9
+            // find which radio button is selected
+            checkedId = radioGroup9.getCheckedRadioButtonId();
+            selected = radioGroup9.findViewById(checkedId);
+            selectedAnswer = radioGroup9.indexOfChild(selected);
+            if (selectedAnswer == answer9) {
+                scoreSummary += score;
+                disableBtn(radioGroup9);
+            } else disableBtn(radioGroup9);
+            // Call function to display the score
+            hasRated = !hasRated;
+            clicked(scoreSummary);
         } else {
-            checkBoxSteel.setEnabled(false);
-            checkBoxGold.setEnabled(false);
-            checkBoxIron.setEnabled(false);
-            checkBoxCopper.setEnabled(false);
+            Toast toast = Toast.makeText(this, "You Have To Reset First", Toast.LENGTH_SHORT);
+            toast.show();
+            Toast toast2 = Toast.makeText(this, "Your score was", Toast.LENGTH_SHORT);
+            toast2.show();
+            clicked(scoreSummary);
         }
-        // Question 4
-        // find which radio button is selected
-        checkedId = radioGroup4.getCheckedRadioButtonId();
-        selected = radioGroup4.findViewById(checkedId);
-        selectedAnswer = radioGroup4.indexOfChild(selected);
-        if (selectedAnswer == answer4) {
-            scoreSummary += score;
-            disableBtn(radioGroup4);
-        } else disableBtn(radioGroup4);
-        // Question 5
-        // Compare Input string with correct answer
-        EditText text = (EditText) findViewById(R.id.question5_input);
-        String question5Input = text.getText().toString();
-        String correctAnswer5 = getResources().getString(R.string.question_5a);
-        if (question5Input.equals(correctAnswer5)) {
-            scoreSummary += score;
-            question5_input.setEnabled(false);
-        } else question5_input.setEnabled(false);
-        // Question 6
-        // find which radio button is selected
-        checkedId = radioGroup6.getCheckedRadioButtonId();
-        selected = radioGroup6.findViewById(checkedId);
-        selectedAnswer = radioGroup6.indexOfChild(selected);
-        if (selectedAnswer == answer6) {
-            scoreSummary += score;
-            disableBtn(radioGroup6);
-        } else disableBtn(radioGroup6);
-        // Question 7
-        // find which radio button is selected
-        checkedId = radioGroup7.getCheckedRadioButtonId();
-        selected = radioGroup7.findViewById(checkedId);
-        selectedAnswer = radioGroup7.indexOfChild(selected);
-        if (selectedAnswer == answer7) {
-            scoreSummary += score;
-            disableBtn(radioGroup7);
-        } else disableBtn(radioGroup7);
-        // Question 8
-        // find which radio button is selected
-        checkedId = radioGroup8.getCheckedRadioButtonId();
-        selected = radioGroup8.findViewById(checkedId);
-        selectedAnswer = radioGroup8.indexOfChild(selected);
-        if (selectedAnswer == answer8) {
-            scoreSummary += score;
-            disableBtn(radioGroup8);
-        } else disableBtn(radioGroup8);
-        // Question 9
-        // find which radio button is selected
-        checkedId = radioGroup9.getCheckedRadioButtonId();
-        selected = radioGroup9.findViewById(checkedId);
-        selectedAnswer = radioGroup9.indexOfChild(selected);
-        if (selectedAnswer == answer9) {
-            scoreSummary += score;
-            disableBtn(radioGroup9);
-        } else disableBtn(radioGroup9);
-        // Call function to display the score
-        clicked(scoreSummary);
     }
 
     // Function to disable Btn
@@ -166,6 +177,7 @@ public class MainActivity extends AppCompatActivity {
         float scorePercent = (scoreSummaryFloat / numberOfQuestions) * 100;
         Toast toast = Toast.makeText(this, "" + scoreSummary + " Point(s) or " + scorePercent + "%", Toast.LENGTH_LONG);
         toast.show();
+
     }
 
     // Function to reset the app
@@ -188,7 +200,8 @@ public class MainActivity extends AppCompatActivity {
         scoreSummary = 0;
         question5_input.setEnabled(true);
         question5_input.setText("");
+        hasRated = !hasRated;
         Toast toast = Toast.makeText(this, "Progress is reset", Toast.LENGTH_LONG);
-
+        toast.show();
     }
 }
